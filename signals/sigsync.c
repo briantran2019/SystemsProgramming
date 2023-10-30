e48751fa233e5ecef96bcd353d1e15996ce8e34a
briantran2019 <briantran2019@gmail.com>
2023-10-30 16:05:13 -0500

    added to sigsync.py, need to do the 3rd part of assignment

--- signals/sigsync.c
+++ signals/sigsync.c
@@ -1,14 +1,42 @@
 #include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void signal_handler(int signum) { 
+    printf("This is a signal handler!\n");
+}
+
+void signal_handler_parent(int signum) {
+    printf("Parent started... \n");
+    sleep(3);
+    printf("Parent about to signal child\n");
+}
 
 int main () {
     int how = SIG_BLOCK;
     sigset_t sigusr = SIGUSR1;
-    int* oldset = NULL;
-    sigprocmask(how, sigusr, oldset);
-    
+    sigset_t* oldset = NULL;
+    sigprocmask(how, &sigusr, oldset);
 
+    signal(SIGUSR1, signal_handler);
+    raise(SIGUSR1);
 
+    pid_t pid = fork();
 
+    if (pid < 0) {
+        printf("fork() error\n");
+        exit(1);
+    }
+    else if (pid != 0) {
+        signal(SIGUSR1, signal_handler_parent);
+    }
+    /* pid_t p = fork();
+    if(p<0){
+      perror("fork fail");
+      exit(1);
+    }
+    printf("Hello world!, process_id(pid) = %d \n", getpid());
+    printf("PPID: %d \n", getppid()); */
     return 0;
 }
